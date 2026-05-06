4918858ac4c0c797421d157985919b0a51cb31f4
GenRevo89 <krishnapatel@mac.com>
2026-05-06 00:31:56 -0600

    v1.0.1l - Google Tag Manager Added.

--- src/app/layout.js
+++ src/app/layout.js
@@ -143,9 +143,30 @@ export default function RootLayout({ children }) {
             src="https://www.facebook.com/tr?id=1983094425931388&ev=PageView&noscript=1"
           />
         </noscript>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P4GCPZ2V');
+            `
+          }}
+        />
       </head>
       <body className={`${playfair.variable} ${lato.variable} font-sans antialiased min-h-screen flex flex-col`}
         style={{ fontFamily: "var(--font-lato), var(--font-primary)", color: "var(--brown-700)", backgroundColor: "var(--warm-cream)" }}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P4GCPZ2V"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Navbar />
         <div className="flex-grow">
           {children}
